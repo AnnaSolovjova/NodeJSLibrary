77c1b1359fa869203646753ac471568562e8a5f9
anso <annsolovjova@gmail.com>
2019-12-11 16:40:55 +0000

    Adding the bootstrap and jquery. Adding static files to serve them to the page through express

--- library/app.js
+++ library/app.js
@@ -1,14 +1,25 @@
 var express = require('express');
+var chalk = require('chalk'); // Coloring console messages e.g chalk.green(string)
+var debug = require('debug')('app');//set DEBUG=app & node app.js
+var morgan = require('morgan');//Web traffic log 
+var path = require('path');
 
 //initialise express
 var app = express();
 
+//Middlewares
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules','bootstrap','dist', 'css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules','bootstrap','dist', 'js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules','jquery','dist')));
+
 //When you get request to this route execute this function
 app.get('/', function(req, res) {
-    res.send('Hello from my library app');
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));//__dirname location of current executable
 })
 
 //listen on the port
 app.listen(3000, function() {
-    console.log('Listening on port 3000');
+    debug(`Listening on port ${chalk.green('3000')}`);
 });
