e4d697d32d36e5d5f7ada998766305aadb328eda
anso <annsolovjova@gmail.com>
2019-12-27 11:23:34 +0000

    add eslint script. To run npm run lint

--- library/app.js
+++ library/app.js
@@ -1,28 +1,27 @@
-var express = require('express');
-var chalk = require('chalk'); // Coloring console messages e.g chalk.green(string)
-var debug = require('debug')('app');//set DEBUG=app & node app.js
-var morgan = require('morgan');//Web traffic log 
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk'); // Coloring console messages e.g chalk.green(string)
+const debug = require('debug')('app'); // set DEBUG=app & node app.js
+const morgan = require('morgan'); // Web traffic log
+const path = require('path');
+// initialise express
+const app = express();
 
-//initialise express
-var app = express();
-
-//Middlewares
+// Middlewares
 app.use(morgan('combined'));
-//Look for styles and scripts in here first 
-//express.static lets node.js know where to look for static files 
+// Look for styles and scripts in here first
+// express.static lets node.js know where to look for static files
 app.use(express.static(path.join(__dirname, 'public')));
-//for static files that we get through npm use these folders - easy to update versions
-app.use('/css', express.static(path.join(__dirname, 'node_modules','bootstrap','dist', 'css')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules','bootstrap','dist', 'js')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules','jquery','dist')));
+// For static files that we get through npm use these folders - easy to update versions
+app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
 
-//When you get request to this route execute this function
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));//__dirname location of current executable
-})
+// When you get request to this route execute this function
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));// __dirname location of current executable
+});
 
-//listen on the port
-app.listen(3000, function() {
-    debug(`Listening on port ${chalk.green('3000')}`);
+// Listen on the port
+app.listen(3000, () => {
+  debug(`Listening on port ${chalk.green('3000')}`);
 });
